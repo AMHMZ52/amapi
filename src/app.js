ea90d0539080f476938ee8732b8c5b1510af0aff
AMHMZ52 <hmzamine10@gmail.com>
2025-11-12 03:15:04 +0100

    Add ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send(`AM API Service...`)
-})
+  res.status(200).send('AM API Service...');
+});
 
 export default app;
